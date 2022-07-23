10b4965314ee6862d9355ef2fba3655a9c4f574e
dileepreddyk <karrolladileep@gmail.com>
2022-07-23 23:27:58 +0530

    Updates

---
+++ precache-manifest.91a2cbbb8ae687c4be7a89c53df1082e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d07f3958b9f626f1f82b0cc3b1c597b3",
+    "url": "/CRUD-app-with-React-Hooks/index.html"
+  },
+  {
+    "revision": "d967111118ddfa918ea2",
+    "url": "/CRUD-app-with-React-Hooks/static/css/main.e53fc632.chunk.css"
+  },
+  {
+    "revision": "97f96de1de5d24b74ebb",
+    "url": "/CRUD-app-with-React-Hooks/static/js/2.0e8c6fb3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/CRUD-app-with-React-Hooks/static/js/2.0e8c6fb3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d967111118ddfa918ea2",
+    "url": "/CRUD-app-with-React-Hooks/static/js/main.9107dc71.chunk.js"
+  },
+  {
+    "revision": "f966a05aedad72499064",
+    "url": "/CRUD-app-with-React-Hooks/static/js/runtime-main.5aa98c5e.js"
+  }
+]);
